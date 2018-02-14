91603e5daaebaf7cdd52ec8420e6708bed1f6cb7
pgshr <pgshr@LAPTOP-2ECK4H22>
2018-02-14 23:01:00 +0530

    Revert to before repository pattern

---
+++ DxMDB/Models/Actor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DxMDB.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [NameFormatValidation]
+        public string Name { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MMM/yyyy}")]
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Bio { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+
+        public Actor()
+        {
+            Movies = new HashSet<Movie>();
+        }
+    }
+}
---
+++ DxMDB/Models/Producer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DxMDB.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [NameFormatValidation]
+        public string Name { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MMM/yyyy}")]
+        [DataType(DataType.Date)]
+        [Display(Name="Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Bio { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
